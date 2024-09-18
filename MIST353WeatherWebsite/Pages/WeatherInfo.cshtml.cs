71da559b7d155b6e060af0002104b2ddac17e131
cpmarchitto <cm00071@mix.wvu.edu>
2024-09-17 20:37:19 -0400

    Added a button to WeatherInfo page

---
+++ MIST353WeatherWebsite/Pages/WeatherInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST353WeatherWebsite.Pages
+{
+    public class WeatherInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
