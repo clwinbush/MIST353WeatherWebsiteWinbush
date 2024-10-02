ef260fdbdef3abeb3a6ec0867b9593c900ae4636
maxofsteel616 <156019436+maxofsteel616@users.noreply.github.com>
2024-10-02 17:41:53 -0400

    Add files via upload
    added my stored procedures

---
+++ MIST353WeatherWebsite/SQL/MaxsonLantzSPs.sql
@@ -0,0 +1,36 @@
+use LandscapingDB
+go
+
+-- add new user
+create or alter proc spCreateUser
+    @FirstName NVARCHAR(50) =NULL,
+    @LastName NVARCHAR(50) =NULL,
+    @Email NVARCHAR(100) =NULL,
+    @PhoneNumber NVARCHAR(20) = NULL,
+    @LocationID INT = NULL
+AS
+BEGIN
+    INSERT INTO Users (FirstName, LastName, Email, PhoneNumber, LocationID)
+    VALUES (@FirstName, @LastName, @Email, @PhoneNumber, @LocationID);
+END
+go
+
+/*
+exec spCreateUser Ted, Kord, [bluebeetle2@kord.com], [555-5555], 1
+go
+*/
+
+-- delete user
+create or alter proc spDeleteUser
+	@UserID int
+AS
+begin
+DELETE FROM Users where USERID=@UserID;
+end
+go
+
+/*
+exec spDeleteUser 12
+go
+*/
+
