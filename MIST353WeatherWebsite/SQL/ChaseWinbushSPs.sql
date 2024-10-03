71a9f4209796a7d6ec05864bf5ce656f7ce6d374
clwinbush <156019299+clwinbush@users.noreply.github.com>
2024-10-02 21:42:14 -0400

    Added Chase's SQL

---
+++ MIST353WeatherWebsite/SQL/ChaseWinbushSPs.sql
@@ -0,0 +1,25 @@
+use LandscapingDB
+GO
+
+-- add new plant
+create or alter proc spCreatePlant
+    @PlantID int =NULL,
+    @PlantName NVARCHAR(100) =NULL,
+    @ScientificName NVARCHAR(150) =NULL,
+    @Description NVARCHAR(500) = NULL,
+    @ClimateID INT = NULL
+AS
+BEGIN
+    INSERT INTO dbo.Plants (PlantID, PlantName, ScientificName, Description, ClimateID)
+    VALUES (@PlantID, @PlantName, @ScientificName, @Description, @ClimateID);
+END
+go
+
+--delete plant
+create or alter proc spDeletePlant
+	@PlantID int
+AS
+begin
+DELETE FROM dbo.Plants where PlantID=@PlantID;
+end
+go
