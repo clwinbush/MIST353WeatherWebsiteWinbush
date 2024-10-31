47e1a8de522ba1c7b9feb61be7336fbbf87fc84a
willrburge <156019241+willrburge@users.noreply.github.com>
2024-10-31 14:54:02 -0400

    Added comments

--- MIST353WeatherWebsiteAPIS/New Repositories/IWillBurgeService.cs
+++ MIST353WeatherWebsiteAPIS/New Repositories/IWillBurgeService.cs
@@ -4,7 +4,10 @@ namespace MIST353WeatherWebsiteAPIS.New_Repositories
 {
     public interface IWillBurgeService
     {
+        //Implements "Update user" into the interface.
         Task<int> UpdateUserAsync(User user);
+
+        //Implements "Get all plants by location" into the interface.
         Task<IEnumerable<Plant>> GetAllPlantsByLocationAsync(int LocationId);
     }
 }
