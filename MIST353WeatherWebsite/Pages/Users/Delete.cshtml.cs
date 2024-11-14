ab1b9c79fd5711e26b66682adf36254a13e07e62
maxofsteel616 <mcl00008@mix.wvu.edu>
2024-11-14 14:17:13 -0500

    added users page

---
+++ MIST353WeatherWebsite/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MIST353WeatherWebsite.Data;
+
+namespace MIST353WeatherWebsite.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MIST353WeatherWebsite.Data.LandscapingDbContext _context;
+
+        public DeleteModel(MIST353WeatherWebsite.Data.LandscapingDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.UserId == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                User = user;
+                _context.Users.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
